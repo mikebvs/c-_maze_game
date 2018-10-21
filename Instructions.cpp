a249edc2191c43e87f933830dcc19551f5209358
Mike Van Sickle <vansimi@bilbo.cs.longwood.edu>
2018-10-20 22:37:12 -0400

    Instructions fix

--- Instructions.cpp
+++ Instructions.cpp
@@ -23,7 +23,7 @@ void Instructions::draw(Graphics* window){
 
 
     window->setColor("WHITE");
-    vector<string> vec = {"Move using w,a,s,d","Select a small, medium, or large map","Coins look like $","Traps look like ^","Your goal is to find the F at the end of the maze","Use m to open the shop while in game", "You can save and load at any point","You have a limited amount of health use it wisely","You start the game with 2 bombs and can buy more with coins", "", "[Q]UIT TO MAIN MENU"};
+    vector<string> vec = {"Move using w,a,s,d","Select a small, medium, or large map","Coins look like: $","Traps look like: ^","Your goal is to find the \"F\" at the end of the maze","Use \"M\" to open the shop while in game", "You can save and load at any point in the Pause Menu","You have a limited amount of health, use it wisely","You start the game with 2 bombs and can buy more with coins", "", "[Q]UIT TO MAIN MENU"};
     for(int i = 0; i<vec.size(); ++i){
         string todraw = vec[i];
         window->drawString(width/2-(vec[i].size()/2),height/2-3+i,todraw);
